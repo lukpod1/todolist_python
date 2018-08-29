124e8e11d38c0cd9d118145149f39fbf30058042
lucaspod <lukasalvespod1@gmail.com>
2018-08-29 07:03:22 -0300

    adicionado melhorias

--- todo_list.py
+++ todo_list.py
@@ -1,47 +1,48 @@
-import os
-
-
-def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 
 listTask = []
-
 
 def addTasks():
     task = input("Digite uma tarefa: ")
     listTask.append(task)
-    cls()
+
+    ask = input("Deseja adicionar outra tarefa? (1:Y) (2:N)\n")
+    if ask == "1":
+        addTasks()
+    else:
+        menu()
 
 
 def rmvTasks():
     task = input("Digite a tarefa que deseja remover: ")
-    listTask.remove(task)
-    cls()
+
+    if(task in listTask):
+        listTask.remove(task)
+    else:
+        print("tarefa não existe, tente novamente")
+        rmvTasks()
 
 
 def showTask():
-    print(listTask)
+    for showlist in listTask:
+        print(showlist)
 
+def menu():
+
+    play = True
+    while play == True:
+        op = input("Menu: (1:add) (2:remove) (3:show list) (4:sair)\n")
+        if op == "1":
+            addTasks()
+
+        elif op == "2":
+            rmvTasks()
+        elif op == "3":
+            showTask()
+        else:
+            print("Bye")
+            play = False
+            break
 
 print("---Bem vindo ao todo list---\n")
+menu()
 
-play = True
-while play == True:
-    op = input("Menu: (1:add) (2:remove) (3:show list) (4:sair)\n")
-
-    if op == "1":
-        addTasks()
-        ask = input("Deseja sair? (1:Y) (2:N)\n")
-        cls()
-        if ask == "1":
-            play = False
-        else:
-            play = True
-    elif op == "2":
-        rmvTasks()
-    elif op == "3":
-        showTask()
-    else:
-        print("Bye")
-        play = False
