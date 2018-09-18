daa3f37224301abd489d3be596264f5c0d7929f2
jaicke <jaicke-santos@outlook.com>
2018-09-18 15:40:29 -0300

    Método de edição adiconado

--- todo_list.py
+++ todo_list.py
@@ -1,10 +1,13 @@
+#Iniciando lista de tarefas vazia
 listTask = []
 
+#Dicionário de usuario
 listUser = {
     "user": "teste",
     "pass": "123"
 }
 
+#Função principal
 def main():
     print("---Bem vindo ao todo list---\n")
 
@@ -18,6 +21,7 @@ def main():
     else:
         pass
 
+#Função de login
 def login():
     print("LOGIN\n")
 
@@ -32,7 +36,7 @@ def login():
         print("Dados invalidos, tente novamente.")
         login()
 
-
+#Função de cadastro
 def cadastrar():
     print("Cadastro no Todo\n")
 
@@ -44,39 +48,56 @@ def cadastrar():
     listUser["senha"] = senha
 
     confir = input("Deseja confirmar seus dados? (1:SIM) (2:NÂO)\n")
-    if confir == "1":
+    if confir == "1" or confir == "sim":
         main()
     else:
         cadastrar()
 
+#Menu principal
 def menu():
     print("MENU:\n")
     play = True
     while play == True:
-        op = input("Menu: (1:add) (2:remove) (3:show list) (4:sair)\n")
+        op = input("Menu: (1:Adicionar) (2:Remover) (3:Editar) (4:Mostrar Lista) (5:sair)\n")
         if op == "1":
             addTasks()
-
         elif op == "2":
             rmvTasks()
         elif op == "3":
+            editTask()
+        elif op == "4":
             showTask()
-        else:
+        elif op == "5":
                 # play = False
             print("Bye")
+        else:
+            print("Opção inválida")
         break
 
+#Menu de edição das tarefas
+def menuEdicao():
+    op = input("(1:Cancelar) (2:Editar outro)")
+    if(op=="1"):
+        menu()
+    elif(op=="2"):
+        editTask()
+    else:
+        print("Opção inválida")
+
+#Adiciona a tarefa
 def addTasks():
     task = input("Digite uma tarefa: ")
     listTask.append(task)
 
     ask = input("Deseja adicionar outra tarefa? (1:Y) (2:N)\n")
-    if ask == "1":
+    if ask == "1" or ask =="y":
         addTasks()
-    else:
+    elif ask == "2" or ask=="n":
         menu()
+    else:
+        print("Opção inválida")
 
-
+#Remove tarefa
 def rmvTasks():
     task = input("Digite a tarefa que deseja remover: ")
 
@@ -87,7 +108,21 @@ def rmvTasks():
         print("tarefa não existe, tente novamente")
         rmvTasks()
 
+#Edita tarefa
+def editTask():
+    task = input("Digite a tarefa que deseja editar: ")    
+    if(task in listTask):
+        taskEdit = input("Tarefa: " + task +"\nDigite a nova versão: ")
+        indexItem = listTask.index(task)
+        listTask.remove(task)
+        listTask.insert(indexItem, taskEdit)
+        menuEdicao()
+    else:
+        print("Tarefa não existe, tente novamente")
+        editTask()
 
+
+#Lista tarefas
 def showTask():
     print("Lista de Tarefas:\n")
     for showlist in listTask:
